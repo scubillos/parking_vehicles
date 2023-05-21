36afb983423c6f92fca5e9db2ba822f7844cc394
Sebastian Cubillos <cbasqbll95@gmail.com>
2023-05-21 15:18:21 -0500

    Avances API Vehiculos

--- app/main.py
+++ app/main.py
@@ -1,39 +1,84 @@
-from typing import Union
+import os
 
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
+from pydantic.types import Optional
+from sqlalchemy import create_engine, MetaData, Table, Column
+from sqlalchemy.sql.sqltypes import Integer, String
+from pydantic import BaseModel
 
-from config.migrations.vehicles_migration import migrate
+from dotenv import load_dotenv
 
+load_dotenv()
+
+# Init FastAPI
 app = FastAPI()
+router = APIRouter(prefix='/api')
 
+db_host = os.getenv("DB_HOST")
+db_port = os.getenv("DB_PORT")
+db_user = os.getenv("DB_USER")
+db_password = os.getenv("DB_PASSWORD")
+db_name = os.getenv("DB_NAME")
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+# Database
+str_conn = "mysql+pymysql://"+db_user+":"+db_password+"@"+db_host+":"+db_port+"/"+db_name
+engine = create_engine(str_conn)
 
+meta = MetaData()
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {
-        "item_id": item_id, "q": q
+# Database Model
+Vehicle = Table(
+    "vehicle",
+    meta,
+    Column("id", Integer, primary_key=True),
+    Column("plat_number", String(10)),
+    Column("description", String(100)),
+    Column("status", Integer),
+    Column("type", String(10))
+)
+
+meta.create_all(engine)
+conn = engine.connect()
+
+# Models
+class VehicleModel(BaseModel):
+    id: Optional[int]
+    plat_number: str
+    description: str
+    status: Optional[int]
+    type: str
+
+# Routes
+@router.get("/vehicle")
+async def get_all():
+    items = conn.execute(Vehicle.select()).fetchall()
+    return items
+
+@router.get("/vehicle/{vehicle_id}")
+async def get(vehicle_id: int):
+    result = conn.execute(
+        Vehicle.select().where(Vehicle.c.id == vehicle_id)
+    ).first()
+    return result
+
+@router.post("/vehicle")
+async def create(vehicle_obj: VehicleModel):
+    new_vehicle = {
+        "plat_number": vehicle_obj.plat_number,
+        "description": vehicle_obj.description,
+        "status": 1,
+        "type": vehicle_obj.type,
     }
 
-@app.get("/migration")
-def migration():
-    migrate()
+    result = conn.execute(Vehicle.insert().values(new_vehicle))
+    conn.commit()
+    response = conn.execute(
+        Vehicle.select().where(Vehicle.c.id == result.lastrowid)
+    ).first()
 
+    return response
 
-
-@app.get("/vehicles")
-def index():
-    return [
-        {'a': 'Item 1'},
-        {'b': 'Item 2'},
-        {'c': 'Item 3'},
-        {'d': 'Item 4'},
-    ]
-
-# conn = connection.conn
+app.include_router(router)
 
 # TODO: Consultar todos los vehiculos [GET]
 # TODO: Crear vehiculos [POST]
--- app/services/vehicles.py
+++
@@ -1,7 +0,0 @@
-def get_all ():
-    return [
-        {"a": "Volvi4"},
-        {"b": "Volvi2"},
-        {"c": "Volvi3"},
-        {"d": "Volvi1"},
-    ]
